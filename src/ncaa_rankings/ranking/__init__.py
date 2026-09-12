0e6cab3d0538f2cf24b66379027e0781d45efc59
larrybillinger <larrybillinger@users.noreply.github.com>
2026-09-12 13:35:17 -0500

    v0.2.1 expose season-only weekly engine

--- src/ncaa_rankings/ranking/__init__.py
+++ src/ncaa_rankings/ranking/__init__.py
@@ -1,3 +1,4 @@
 from .engine import RecursiveRankingEngine
+from .weekly import WeeklySeasonRankingEngine
 
-__all__ = ["RecursiveRankingEngine"]
+__all__ = ["RecursiveRankingEngine", "WeeklySeasonRankingEngine"]
