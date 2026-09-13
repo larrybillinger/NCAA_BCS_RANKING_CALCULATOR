cc8be46708570c6348bf4176a267d6683faa1bda
larrybillinger <larrybillinger@users.noreply.github.com>
2026-09-13 00:35:43 -0500

    Export live Division I weighted model

--- src/ncaa_rankings/models/__init__.py
+++ src/ncaa_rankings/models/__init__.py
@@ -1,8 +1,10 @@
 from .base import GameScore, RankingResult, Site, TeamGame, TeamStanding
+from .division_i_weighted import DivisionIWeightedModel
 from .legacy_fbs import LegacyFBSModel
 from .modern_legacy import ModernLegacyModel
 
 __all__ = [
+    "DivisionIWeightedModel",
     "GameScore",
     "LegacyFBSModel",
     "ModernLegacyModel",
