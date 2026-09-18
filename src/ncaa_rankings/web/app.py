7715044fbd1d92b5baec3a5f2f69e560691e0856
larrybillinger <larrybillinger@users.noreply.github.com>
2026-09-17 22:43:54 -0500

    Bump web app to 0.6.0

--- src/ncaa_rankings/web/app.py
+++ src/ncaa_rankings/web/app.py
@@ -50,7 +50,7 @@ async def lifespan(app: FastAPI):
 
 
 settings = get_settings()
-app = FastAPI(title=settings.web_title, version="0.5.2", lifespan=lifespan)
+app = FastAPI(title=settings.web_title, version="0.6.0", lifespan=lifespan)
 app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
 
 
